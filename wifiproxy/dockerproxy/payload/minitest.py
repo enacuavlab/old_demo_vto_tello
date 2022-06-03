a23cccab514c566a8b3ba51dccae3596d7ded122
pprz <amtcvx@gmail.com>
2022-06-03 15:55:01 +0200

    up

--- wifiproxy/dockerproxy/payload/minitest.py
+++ wifiproxy/dockerproxy/payload/minitest.py
@@ -7,27 +7,28 @@ import queue
 #------------------------------------------------------------------------------
 #------------------------------------------------------------------------------
 class thread_monitor_batt(threading.Thread):
-  def __init__(self,sock,commands):
+  def __init__(self,addGCS,sockGCS,sockDrone,commands):
     threading.Thread.__init__(self)
-    self.sock = sock
+    self.addGCS = addGCS
+    self.sockGCS = sockGCS
+    self.sockDrone = sockDrone
     self.commands = commands
     self.running = True
 
   def run(self):
-    sockOut = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     msg='battery?'
     while self.running:
       if (msg not in self.commands.queue): self.commands.put(msg)
       try:
-        data, server = self.sock.recvfrom(1518)
+        data, server = self.sockDrone.recvfrom(1518)
         tmp=data.decode(encoding="utf-8")
         if(tmp.count('\n')==1):
           batt=tmp[:-1]
           print(batt)
-          sockOut.sendto(batt.encode(encoding="utf-8"),('172.17.0.2',8889))
+          self.sockGCS.sendto(batt.encode(encoding="utf-8"),self.addGCS)
           time.sleep(1)
 
-      except socket.timeout:
+      except .encode(encoding="utf-8")socket.timeout:
         pass
 
     print("Thread batt stopped")
@@ -35,14 +36,43 @@ class thread_monitor_batt(threading.Thread):
 
 #------------------------------------------------------------------------------
 #------------------------------------------------------------------------------
+class thread_command(threading.Thread):
+  def __init__(self,sockGCS,commands):
+    threading.Thread.__init__(self)
+    self.sockGCS = sockGCS
+    self.commands = commands
+    self.running = True
+
+  def run(self):
+    while self.running:
+      try:
+        data, server = self.sockGCS.recvfrom(1518)
+        tmp=data.decode(encoding="utf-8")
+        print(tmp)
+
+      except socket.timeout:
+        pass
+
+    print("Thread command stopped")
+
+
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
 if __name__ == '__main__':
-  sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-  sock.settimeout(1.0)
+
+  sockGCS = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  addGCS = ('172.17.0.1',8889)
+
+  sockDrone = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+  addDrone = ('192.168.10.1',8889)
+  sockDrone.bind (('192.168.10.2',8889))
+  sockDrone.settimeout(1.0)
 
   commands = queue.Queue()
   commands.put('command')
 
-  threadBatt = thread_monitor_batt(sock,commands)
+  threadBatt = thread_monitor_batt(addGCS,sockGCS,sockDrone,commands)
+  threadCmd = thread_command(sockGCS,commands)
   threadBatt.start()
 
   try:
@@ -51,14 +81,15 @@ if __name__ == '__main__':
         print(list(commands.queue))
         msg=commands.get()
         print("Sending <"+msg+">")
-        sock.sendto(msg.encode(encoding="utf-8"),('192.168.10.1',8889))
-
+        sockDrone.sendto(msg.encode(encoding="utf-8"),addDrone)
 
       time.sleep(0.1)
 
   except KeyboardInterrupt:
+    threadCmd.running = False
     threadBatt.running = False
     time.sleep(1)
-    sock.close()
+    sockDrone.close()
+    sockGCS.close()
     print("mainloop stopped")
 
--- wifiproxy/test.py
+++ wifiproxy/test.py
@@ -2,6 +2,7 @@
 import socket
 import time
 import threading
+import queue
 
 #------------------------------------------------------------------------------
 #------------------------------------------------------------------------------
@@ -12,13 +13,12 @@ class thread_monitor_batt(threading.Thread):
     self.running = True
 
   def run(self):
+    print("Thread batt started")
     while self.running:
       try:
         data, server = self.sock.recvfrom(1518)
-        tmp=data.decode(encoding="utf-8")
-        if(tmp.count('\n')==1):
-          batt=tmp[:-1]
-          print(batt)
+        batt=data.decode(encoding="utf-8")
+        print(batt)
 
       except socket.timeout:
         pass
@@ -27,21 +27,72 @@ class thread_monitor_batt(threading.Thread):
 
 
 #------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+class thread_mission(threading.Thread):
+  def __init__(self,commands):
+    threading.Thread.__init__(self)
+    self.commands = commands
+    self.running = True
+
+  def run(self):
+    for i in range(5): 
+      if self.running:time.sleep(1)
+    if self.running: self.commands.put('takeoff')
+    for i in range(8): 
+      if self.running:time.sleep(1)
+    if self.running: self.commands.put('land')
+    print("Thread mission stopped")
+
+
+#------------------------------------------------------------------------------
+#------------------------------------------------------------------------------
+class thread_startup(threading.Thread):
+  def __init__(self,commands):
+    threading.Thread.__init__(self)
+    self.commands = commands
+    self.running = True
+
+  def run(self):
+    if self.running:
+      self.commands.put('command')
+      self.commands.put('streamon')
+      self.commands.put('downvision 0')
+    print("Thread startup stopped")
+
+
+#------------------------------------------------------------------------------
 if __name__ == '__main__':
 
   sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-  sock.bind(('172.17.0.1',8889))
+  tello_add=('172.17.0.1',8889)
+  sock.bind(tello_add)
   sock.settimeout(1.0)
 
+  commands = queue.Queue()
+  commands.put('command')
+
   threadBatt = thread_monitor_batt(sock)
+  #threadMiss = thread_mission(commands)
+  threadStart = thread_startup(commands)
+  threadBatt.start()
+  #threadMiss.start()
+  threadStart.start()
 
   try:
     while True:
+      while not commands.empty():
+        print(list(commands.queue))
+        msg=commands.get()
+        print("Sending <"+msg+">")
+        sock.sendto(msg.encode(encoding="utf-8"),tello_add)
+
       time.sleep(0.1)
 
 
   except KeyboardInterrupt:
     print("\nWe are interrupting the program\n")
+    threadStart.running = False
+#    threadMiss.running = False
     threadBatt.running = False
     time.sleep(1)
     sock.close()
