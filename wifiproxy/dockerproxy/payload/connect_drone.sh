922c4e0eaebc1ff669cf244c18a5fca05035077b
pprz <amtcvx@gmail.com>
2022-06-07 11:30:35 +0200

    up

--- wifiproxy/dockerproxy/payload/connect_drone.sh
+++ wifiproxy/dockerproxy/payload/connect_drone.sh
@@ -5,21 +5,20 @@ cp /sbin/dhclient /usr/sbin/dhclinet >/dev/null
 ifconfig $WIFI_DEV down
 ifconfig $WIFI_DEV up
 
-while [[ "`iw dev $WIFI_DEV scan 2>/dev/null | grep $DRONE_AP | wc -l`" != 1 ]];do sleep 0.1;done
-iw dev $WIFI_DEV connect $DRONE_AP
-/usr/sbin/dhclinet $WIFI_DEV > /dev/null 2>&1
-pkill dhclient
-WIFI_IP=$(ip a | grep $WIFI_DEV | pcregrep -o1 'inet ([0-9]+.[0-9]+.[0-9]+.[0-9]+)')
-
-/usr/bin/socat udp-recv:11111,bind=192.168.10.2 udp-sendto:172.17.0.1:$VID_PORT &>/dev/null &
-
-while true;
-do 
-  while [[ $(cat /sys/class/net/$WIFI_DEV/carrier) = 1 ]];do sleep 0.1;done
+while true
+do
   while [[ "`iw dev $WIFI_DEV scan 2>/dev/null | grep $DRONE_AP | wc -l`" != 1 ]];do sleep 0.1;done
+
   iw dev $WIFI_DEV connect $DRONE_AP
-  /usr/bin/killall /usr/sbin/dhclinet > /dev/null 2>&1
   /usr/sbin/dhclinet $WIFI_DEV > /dev/null 2>&1
-  /usr/bin/killall /usr/bin/python3 > /dev/null 2>&1
-  /minitest.py &> /dev/null
+  pkill dhclinet
+
+  WIFI_IP=$(ip a | grep $WIFI_DEV | pcregrep -o1 'inet ([0-9]+.[0-9]+.[0-9]+.[0-9]+)')
+  /usr/bin/socat udp-recv:11111,bind=$WIFI_IP udp-sendto:172.17.0.1:$VID_PORT &>/dev/null &
+  /minitest.py $WIFI_IP &> /dev/null &
+
+  while [[ $(cat /sys/class/net/$WIFI_DEV/carrier) = 1 ]];do sleep 0.1;done
+
+  pkill socat > /dev/null 2>&1
+  pkill minitest.py > /dev/null 2>&1
 done
