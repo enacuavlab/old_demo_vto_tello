ef486ef2151984cbfb3ae20abcf5b70f4190d6f4
amtcvx <xp31@free.fr>
2022-06-27 10:24:24 +0200

    up

--- path_plan/buildingFromFilteredCsv.py
+++ path_plan/buildingFromFilteredCsv.py
@@ -13,12 +13,13 @@ filename = "testFiltered.csv"
 class Building():
   def __init__(self,vertices): # Buildings(obstacles) are defined by coordinates of their vertices.
     self.vertices = vertices
+    
     panels = np.array([])
     rad = 0.2
     safetyfac = 1.1
     rad = rad * safetyfac
-    scale = 1e6
-    pco = pyclipper.PyclipperOffset()
+    scale = 1e6  # Inflate
+    pco = pyclipper.PyclipperOffset() 
     pco.AddPath( (self.vertices[:,:2] * scale).astype(int).tolist() , pyclipper.JT_MITER, pyclipper.ET_CLOSEDPOLYGON)
     inflated  =  np.array ( pco.Execute( rad*scale )[0] ) / scale
     height = self.vertices[0,2]
@@ -28,12 +29,13 @@ class Building():
     angles = np.arctan2( ( Yavg*np.ones(len(points[:,1])) - points[:,1] ) , ( Xavg*np.ones(len(points[:,0])) - points[:,0] ) )
     sorted_angles = sorted(zip(angles, points), reverse = True)
     points_sorted = np.vstack([x for y, x in sorted_angles])
+
     self.vertices = points_sorted
     self.gammas = {}           # Vortex Strenghts
     self.K_inv = None
 
 
-    size=0.01
+    size=0.01 # Panelize
     for index,vertice in enumerate(self.vertices): # Divides obstacle edges into smaller line segments, called panels.
       xyz1 = self.vertices[index]                                 # Coordinates of the first vertice
       xyz2 = self.vertices[ (index+1) % self.vertices.shape[0] ]  # Coordinates of the next vertice
@@ -46,6 +48,7 @@ class Building():
       else:
         panels = np.vstack((panels,np.linspace(xyz1,xyz2,n)[1:])) # Divide the edge into "n" equal segments:
 
+    # Compute coeff matrix for Vortex method
     self.nop = panels.shape[0]    # Number of Panels
     self.pcp = np.zeros((self.nop,2))  # Controlpoints: at 3/4 of panel
     vp  = np.zeros((self.nop,2))  # Vortex point: at 1/4 of panel
@@ -57,13 +60,12 @@ class Building():
     vp   = XYZ2 + (XYZ1-XYZ2)*0.25 # Vortex point at 1/4 of panel.
     self.pb   = np.arctan2( ( XYZ2[:,1] - XYZ1[:,1] ) , ( XYZ2[:,0] - XYZ1[:,0] ) )  + np.pi/2
     K = np.zeros((self.nop,self.nop))
-    for m in range(self.nop ):
+    for m in range(self.nop ): 
       for n in range(self.nop ):
         K[m,n] = ( 1 / (2*np.pi)
                      * ( (self.pcp[m][1]-vp[n][1] ) * np.cos(self.pb[m] ) - ( self.pcp[m][0] - vp[n][0] ) * np.sin(self.pb[m] ) )
                      / ( (self.pcp[m][0]-vp[n][0] )**2 + (self.pcp[m][1] - vp[n][1] )**2 ) )
     self.K_inv = np.linalg.inv(K) # Inverse of coefficient matrix: (Needed for solution of panel method eqn.)
-
 
 
   def gamma_calc(self,vehicle,othervehicles): # Calculates unknown vortex strengths by solving panel method eq.
@@ -79,7 +81,8 @@ class Building():
     for i,othervehicle in enumerate(othervehicles) :
       vel_source[:,0] += (othervehicle.source_strength*(self.pcp[:,0]-othervehicle.position[0]))/(2*np.pi*((self.pcp[:,0]-othervehicle.position[0])**2+(self.pcp[:,1]-othervehicle.position[1])**2))
       vel_source[:,1] += (othervehicle.source_strength*(self.pcp[:,1]-othervehicle.position[1]))/(2*np.pi*((self.pcp[:,0]-othervehicle.position[0])**2+(self.pcp[:,1]-othervehicle.position[1])**2))
-      RHS[:,0]  = -vehicle.V_inf[0]  * np.cos(self.pb[:])  \
+
+    RHS[:,0]  = -vehicle.V_inf[0]  * np.cos(self.pb[:])  \
                   -vehicle.V_inf[1]  * np.sin(self.pb[:])  \
                   -vel_sink[:,0]     * np.cos(self.pb[:])  \
                   -vel_sink[:,1]     * np.sin(self.pb[:])  \
@@ -87,8 +90,7 @@ class Building():
                   -vel_source[:,1]   * np.sin(self.pb[:])  \
                   -vel_source_imag[:,0]  * np.cos(self.pb[:])  \
                   -vel_source_imag[:,1]  * np.sin(self.pb[:])  +vehicle.safety
-      self.gammas[vehicle.ID] = np.matmul(self.K_inv,RHS)
-
+    self.gammas[vehicle.ID] = np.matmul(self.K_inv,RHS)
 
 #--------------------------------------------------------------------------------
 class Vehicle():
@@ -207,7 +209,7 @@ def Flow_Velocity_Calculation(vehicles,buildings):
 
     flow_vels[f,:] = [V_flow[f,0],V_flow[f,1],W_flow[f,0]]
 
-    return flow_vels
+  return flow_vels
 
 #--------------------------------------------------------------------------------
 def display(name,pts):
@@ -254,6 +256,18 @@ def init(buildingList):
       display(item1[0],pts)
 
 
+#def init_test(buildingList):
+#  buildings = [([[3.0, 2.0, 4.2], [2.75, 1.567, 4.2], [2.25, 1.567, 4.2], [2.0, 2.0, 4.2], [2.25, 2.433, 4.2], [2.75, 2.433, 4.2]]),
+#               ([[1.0, 3.0, 4.5], [0.75, 2.567, 4.5], [0.25, 2.567, 4.5], [0.0, 3.0, 4.5], [0.25, 3.433, 4.5], [0.75, 3.433, 4.5]]), 
+#               ([[1.0, 0.5, 4], [0.75, 0.067, 4], [0.25, 0.067, 4], [0.0, 0.5, 4], [0.25, 0.933, 4], [0.75, 0.933, 4]]), 
+#               ([[-2.65, 1.5, 4.5], [-3.0, 1.15, 4.5], [-3.35, 1.5, 4.5], [-3.0, 1.85, 4.5]]),
+#               ([[-2.65, -1.5, 4.5], [-3.0, -1.85, 4.5], [-3.35, -1.5, 4.5], [-3.0, -1.15, 4.5]]),
+#               ([[-1.15, -0.2, 4.5], [-1.5, -0.55, 4.5], [-1.85, -0.2, 4.5], [-1.5, 0.15, 4.5]]), 
+#               ([[1.5, -2.5, 4.2], [1, -2.5, 4.2], [1, -1.4, 4.2], [1.5, -1, 4.2]]),
+#               ([[3.5, -2.5, 4.2], [3, -2.5, 4.2], [3, -1, 4.2], [3.5, -1.4, 4.2]])]
+#  for b in buildings: buildingList.append(Building(np.array(b)))
+
+
 def run(buildingList):
   acDict = {60:[('TELLO-ED4310')],65:[('TELLO-F0B594')]}
   sourceStrength = 0.95 # Tello repelance
@@ -275,6 +289,7 @@ def run(buildingList):
   plt.plot(vehicleList[1].position[0],vehicleList[1].position[1],color='red',marker='o',markersize=12)
   plt.plot(targetPos[0],targetPos[1],color='green',marker='o',markersize=12)
 
+  
   flow_vels = Flow_Velocity_Calculation(vehicleList,buildingList)
   print(flow_vels)
 
