673fc4f0cca738f2e65c795eb53f4ce2cdd206dd
amtcvx <xp31@free.fr>
2022-10-04 16:04:25 +0200

    Update connect_drone.sh

--- wifiproxy/dockerproxy/payload/connect_drone.sh
+++ wifiproxy/dockerproxy/payload/connect_drone.sh
@@ -17,7 +17,11 @@ do
   DOCKER_IP=$(ip a | grep eth0 | pcregrep -o1 'inet ([0-9]+.[0-9]+.[0-9]+.[0-9]+)')
   /minitest.py $DOCKER_IP $CMD_PORT &> /dev/null &
 
-  while timeout 0.5 ping -c 1 -n 192.168.10.1 &> /dev/null; do sleep 0.5; done
+  #while timeout 0.5 ping -c 1 -n 192.168.10.1 &> /dev/null; do sleep 0.5; done
+  REACHEABLE=0;
+  while [ $REACHEABLE -ne "1" ];
+    do ping -q -c 1 -w 2 192.168.10.1 &> /dev/null; REACHEABLE=$?; sleep 1;
+  done
 
   pkill socat > /dev/null 2>&1
   pkill minitest.py > /dev/null 2>&1
