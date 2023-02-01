890d042144ff3424aa99ea215f876876729bce63
pprz <amtcvx@gmail.com>
2023-02-01 11:52:11 +0100

    up

--- path_plan_new/sandbox/experiments/drawing3D.py
+++ path_plan_new/sandbox/experiments/drawing3D.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
 
+import time,psutil
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib.animation as animation
@@ -7,44 +8,9 @@ import matplotlib.animation as animation
 from matplotlib.text import Annotation
 from mpl_toolkits.mplot3d import Axes3D
 
-from mpl_toolkits import mplot3d as plt3d
+from mpl_toolkits.mplot3d import proj3d
 
-from typing import Tuple,Union
-
-https://stackoverflow.com/questions/62463276/how-to-annotate-3d-plot-on-matplotlib
-
-#--------------------------------------------------------------------------------
-class Annotation3D(Annotation):
-
-  def __init__(self,text:str,xyz: Tuple[float, float, float],*args,**kwargs):
-    print("heelo")    
-    Annotation.__init__(self, text, xy=(0, 0), *args, **kwargs)
-    self._xyz = xyz
-    self._xy: Optional[Tuple[float, float]] = None
-
-  @property
-  def xy(self):
-    if self._xy is not None:
-      return self._xy
-    *xy2d, _ = plt3d.proj3d.proj_transform(*self._xyz, self.axes.M)
-    return xy2d
-
-  @xy.setter
-  def xy(self, val):
-    self._xy = val
-
-  @xy.deleter
-  def xy(self):
-    self._xy = None
-
-#--------------------------------------------------------------------------------
-def annotateND(ax: Union[plt.Axes, plt3d.axes3d.Axes3D], text: str, \
-               xy: Tuple[float, ...], *args, **kwargs) -> Union[Annotation3D, Annotation]:
-  if isinstance(ax, plt3d.axes3d.Axes3D):
-    a = Annotation3D(text, xy, *args, **kwargs)
-    ax.add_artist(a)
-    return a
-  return ax.annotate(text, xy, *args, **kwargs)
+#https://stackoverflow.com/questions/62463276/how-to-annotate-3d-plot-on-matplotlib
 
 #--------------------------------------------------------------------------------
 class Drawing3D():
@@ -73,30 +39,37 @@ class Drawing3D():
     self.ax.set_ylim(-5, 5)
     self.ax.set_zlim(0, 10)
 
+    self.store_cpu = (0.0,0.0)
+    self.avg_cpu = 0.0
+    self.store_time = time.time()
+    self.fps_text = self.ax.set_title(label='')
 
-    self.annotations = {}
     self.storepos = {}
     self.plots = {}
-    self.artiststodraw = []
     for elt in vehiclelstsim: 
-      self.annotations[elt.ID]=annotateND(self.ax,"HELLO",xy=(0,0))
-#      self.annotations[elt.ID]=self.ax.annotateND(elt.ID, xy=(0,0),va='center',ha='center')
-#      self.artiststodraw.append(self.annotations[elt.ID])
-      scat = self.ax.plot([], [], [], 'o',markersize=8)[0]
       self.storepos[elt.ID] = np.zeros(3)
-      self.plots[elt.ID]=(self.get_listpos,scat)
-      self.artiststodraw.append(scat)
+      self.plots[elt.ID]=(self.get_listpos,\
+                          self.ax.plot([], [], [], 'o',markersize=8)[0],\
+                          self.ax.text2D(0,0,elt.ID,fontsize=22))
 
     for elt in rigidBodyDict: 
-#      self.annotations[elt]=self.ax.annotate(elt, xy=(0,0),va='center',ha='center')
-#      self.artiststodraw.append(self.annotations[elt])
-      scat = self.ax.plot([], [], [], 'o',markersize=8)[0]
       self.storepos[elt] = np.zeros(3)
-      self.plots[elt]=(self.get_dicpos,scat)
-      self.artiststodraw.append(scat)
-
+      self.plots[elt]=(self.get_dicpos,\
+                       self.ax.plot([], [], [], 'o',markersize=8)[0],\
+                       self.ax.text2D(0,0,elt,fontsize=22))
 
   def update(self,i):
+
+    curr_time = time.time()
+    fps = 1/(curr_time - self.store_time)
+    self.store_time = curr_time
+  
+    self.store_cpu = tuple(np.array(self.store_cpu)+ (1.0,psutil.cpu_percent()))
+    if self.store_cpu[0] == 10:
+      self.avg_cpu =  self.store_cpu[1]/10
+      self.store_cpu = (0.0,0.0)
+
+    self.fps_text.set_text("FPS "+f'{fps:.2f}'+"             CPU "+f'{self.avg_cpu:.2f}')
 
     for elt in self.plots:
       curr=self.plots[elt][0](elt) # call register get function 
@@ -104,12 +77,13 @@ class Drawing3D():
       self.plots[elt][1].set_data([curr[0],prev[0]],[curr[1],prev[1]])
       self.plots[elt][1].set_3d_properties([curr[2],prev[2]])
       self.storepos[elt] = curr
-#      self.annotations[elt].set_position((curr[0],curr[1],curr[2]))
+      x2, y2, _ = proj3d.proj_transform(curr[0],curr[1],curr[2],self.ax.get_proj())
+      self.plots[elt][2].set_position((x2,y2))
 
-      
-    return (self.artiststodraw)
+    return (self.ax,) # coma because of blit=True
 
 
   def start(self):
-    ani = animation.FuncAnimation(self.fig,self.update,interval=1000/self.FPS,blit=True)
+#    ani = animation.FuncAnimation(self.fig,self.update,interval=1000/self.FPS,blit=True)
+    ani = animation.FuncAnimation(self.fig,self.update,interval=1,blit=True)
     plt.show()
